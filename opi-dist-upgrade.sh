45f86ca287d0c546cf21a086b949145c6f9ee869
PA Nilsson <pa@openproducts.se>
2014-08-07 20:25:51 +0200

    Updated script not to run depending on config and mounted sd card Resolved #95

--- opi-dist-upgrade.sh
+++ opi-dist-upgrade.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+def_sdcard="/dev/mapper/opi"
+def_opiloc="/var/opi"
+
+if [ -e /etc/opi/opi-update.conf ]; then 
+	source /etc/opi/opi-update.conf
+	if [ $update != "yes" ]; then
+		echo "Updates not enabled"
+		exit 0
+	fi
+else
+	echo "No config file, exiting"
+	exit 0
+fi
+
+
+
+sd_card=$(sed -n "s%\(${def_sdcard}\)\s${def_opiloc}.*%\1% p" /proc/mounts) # get sd-card device
+if [ -z $sd_card ] || [ ! -b $sd_card ]; then
+	# the backend must be defined and the sd-card device must exist and be mounted
+	echo "update aborted, no sd card found or sd card is not unlocked"
+	exit 0
+fi
+
+echo "Starting update"
 
 apt_output=$( /bin/mktemp -t )
 echo APT-GET Update was run: $( /bin/date +%c ) >> $apt_output

9e622024c319ece4a32f2059e41e75296ef2f1a6
PA Nilsson <pa@openproducts.se>
2014-08-26 15:36:34 +0200

    Removed script ouput in order not to trigger cron to send mail Resolves #158

--- opi-dist-upgrade.sh
+++ opi-dist-upgrade.sh
@@ -3,14 +3,22 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 def_sdcard="/dev/mapper/opi"
 def_opiloc="/var/opi"
 
+verbose=$1
+function report {
+	if [ ! -z $verbose ] && [ $verbose == '-v' ] ; then
+		echo $1
+	fi
+}
+
+
 if [ -e /etc/opi/opi-update.conf ]; then 
 	source /etc/opi/opi-update.conf
 	if [ $update != "yes" ]; then
-		echo "Updates not enabled"
+		report "Updates not enabled"
 		exit 0
 	fi
 else
-	echo "No config file, exiting"
+	report "No config file, exiting"
 	exit 0
 fi
 
@@ -19,11 +27,11 @@ fi
 sd_card=$(sed -n "s%\(${def_sdcard}\)\s${def_opiloc}.*%\1% p" /proc/mounts) # get sd-card device
 if [ -z $sd_card ] || [ ! -b $sd_card ]; then
 	# the backend must be defined and the sd-card device must exist and be mounted
-	echo "update aborted, no sd card found or sd card is not unlocked"
+	report "update aborted, no sd card found or sd card is not unlocked"
 	exit 0
 fi
 
-echo "Starting update"
+report "Starting update"
 
 apt_output=$( /bin/mktemp -t )
 echo APT-GET Update was run: $( /bin/date +%c ) >> $apt_output
@@ -35,20 +43,16 @@ apt-get -q -y -o Dpkg::Options::="--force-confnew" dist-upgrade | grep -i instal
 
 if [ $? -ne 0 ]; then
 	tmpfile=$( /bin/mktemp -t )
-	$recipient=admin.localhost
-	echo "return-path: $recipient" >> $tmpfile
-	echo "for: $recipient" >> $tmpfile
-	echo "from: opi" >> $tmpfile
-	echo "to: $recipient" >> $tmpfile
-	echo "subject: Error running upgrade" >> $tmpfile
+	$recipient=root.localhost
 	echo "An error was encounteded when running the upgrade scripts on your opi." >> $tmpfile
 	echo >> $tmpfile
 
 	cat $apt_output >> $tmpfile
 	echo >> $tmpfile
+
 	echo "If you can not understand this message, please contact support@openproducts.com" >> $tmpfile
 	echo >> $tmpfile
-	#/bin/cat $tmpfile | sendmail -t
+
 	echo $tmpfile
 	rm $tmpfile
 fi

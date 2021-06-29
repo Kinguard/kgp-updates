8d5949a0a97ba0c2f3028f2498bf9369fa3637f0
Tor Krill <tor@openproducts.se>
2021-06-29 11:46:16 +0200

    Refactor to use kgp-bashlibrary for logging
    Signed-off-by: Tor Krill <tor@openproducts.se>

--- scripts/update/opi-dist-upgrade.sh
+++ scripts/update/opi-dist-upgrade.sh
@@ -1,59 +1,63 @@
 #!/bin/bash
+
+LOGNAME="OPI Distupgrade"
+source /usr/share/kgp-bashlibrary/scripts/kgp-logging.sh
+
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 def_sdcard="/dev/mapper/opi"
 def_opiloc="/var/opi"
 
-verbose=$1
-function report {
-	if [ ! -z $verbose ] && [ $verbose == '-v' ] ; then
-		echo $1
-	fi
-}
-
+log_notice "Starting up"
 
 if enabled=$(kgp-sysinfo -p -c autoupdate -k enabled) ; then
 	if [[ $enabled -ne 1 ]] ; then
-		report "Updates disabled"
+		log_notice "Updates disabled"
 		exit 0
 	fi
 else
 	kgp-notifier -q -l "LOG_DEBUG" -m "Missing 'autoupdate->enabled' parameter in 'sysconfig'." -i "sysctrl"
-	report "Missing 'autoupdate->enabled' parameter in 'sysconfig'"
+	log_err "Missing 'autoupdate->enabled' parameter in 'sysconfig'"
 	exit 1
 fi
 
 
 if locked=$(kgp-sysinfo -l); then
-	report "System locked, not possible to run updates."
+	log_notice "System locked, not possible to run updates."
 	exit 0
 fi
 
 if ! grep -iq "'installed'\s\+=>\s\+true" /usr/share/nextcloud/config/config.php
 then
-	report "Update aborted, OC not setup yet"
+	log_notice "Update aborted, OC not setup yet"
 	exit 0
 fi
 
-report "Try free up memory"
+log_debug "Try free up memory"
 
 sync
 echo 3 > /proc/sys/vm/drop_caches
 
-report "Starting update"
+log_info "Starting update"
 
 apt_output=$( /bin/mktemp -t )
 echo APT-GET Update was run: $( /bin/date +%c ) >> $apt_output
 echo >> $apt_output
 apt-get -q -y update > /dev/null
 
+log_debug "update completed"
+
 export DEBIAN_FRONTEND=noninteractive
 apt-get -q -y -o Dpkg::Options::="--force-confnew" dist-upgrade &>> $apt_output
 
+log_debug "upgrade completed"
+
 if [ $? -ne 0 ]; then
+	log_error "Upgrade failed!"
 	apt_log="/var/log/dist-upgrade.log"
 	cp $apt_output $apt_log
 	kgp-notifier -l "LOG_ERR" -m "Upgrade failed, log in $apt_log. Please run upgrade manually from command line." -i "sysctrl"	
 else
+	log_debug "upgrade succeded, perform cleanups"
 	# run cleanups
 	echo "Cleaning apt cache" >> $apt_output
 	apt-get -q -y clean &>> $apt_output
@@ -61,6 +65,7 @@ else
 	apt-get -q -y autoremove &>> $apt_output
 	remove_status=$?
 	if [[ $clean_status -ne 0 || $remove_status -ne 0 ]]; then
+		log_error "cleanup failed!"
 		cleanup_log="/var/log/apt-cleanup.log"
 		cp $apt_output $cleanup_log
 		kgp-notifier -l "LOG_WARNING" -m "apt clean up failed, see log in $cleanup_log" -i "sysctrl"
@@ -71,7 +76,9 @@ fi
 rm $apt_output
 
 if [[ -f /var/run/reboot-required ]]; then
+	log_debug "Reboot requiered"
 	kgp-notifier -q -l "LOG_NOTICE" -m "Reboot required due to updates of core functions." -i "sysctrl"
 	rm /var/run/reboot-required
 fi
+log_notice "Completed"
 exit 0
